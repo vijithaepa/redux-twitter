c35b0471207dc27894db6fec7a042709584f16aa
vijitha epa <vepa@bglcorp.com.au>
2019-07-28 20:28:44 +1000

    implement react loading

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,16 +2,19 @@ import { getInitialData } from "../utils/api";
 import { receiveTweets } from "./tweets";
 import { receiveUsers } from "./users";
 import { setAuthedUser } from "./authedUser";
+import {showLoading, hideLoading} from "react-redux-loading";
 
 const AUTHED_USER_ID = 'tylermcginnis'
 
 export const handleInitialData = () => {
     return (dispatch) => {
+        dispatch(showLoading())
         return getInitialData()
             .then(({users, tweets}) => {
                 dispatch(receiveTweets(tweets))
                 dispatch(receiveUsers(users))
                 dispatch(setAuthedUser(AUTHED_USER_ID))
+                dispatch(hideLoading())
             })
     }
 }
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,13 @@
-import {combineReducers} from 'redux'
+import { combineReducers } from 'redux'
 import authedUser from './authedUser'
 import tweets from './tweets'
 import users from './users'
-
+import { loadingBarReducer } from 'react-redux-loading'
 
 
 export default combineReducers({
-    authedUser, tweets,users
+    authedUser,
+    tweets,
+    users,
+    loadingBar: loadingBarReducer
 })
