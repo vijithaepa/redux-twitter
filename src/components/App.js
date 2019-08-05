5ec42df888b778b43b20cd5c9a42f76d773def4a
vijitha epa <vepa@bglcorp.com.au>
2019-08-05 22:23:43 +1000

    reply to a tweet functionality with hard coded replyTo tweet id

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { handleInitialData } from '../actions/shared'
 import LoadingBar from 'react-redux-loading'
-import NewTweet from "./NewTweet";
+import TweetPage from "./TweetPage";
 
 class App extends Component {
 
@@ -16,7 +16,7 @@ class App extends Component {
                 <LoadingBar/>
                 {this.props.loading === true
                     ? null
-                    : <NewTweet/>}
+                    : <TweetPage match={{params: {id: '6h5ims9iks66d4m7kqizmv'}}}/>}
             </div>
         )
     }
